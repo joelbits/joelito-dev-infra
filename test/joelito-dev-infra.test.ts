b52f4b68062e4248a3e4911df1242220aa51eeaa
joelITo <info@joelito.dev>
2024-09-23 18:18:32 +0200

    Add AWS CDK

---
+++ test/joelito-dev-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as JoelitoDevInfra from '../lib/joelito-dev-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/joelito-dev-infra-stack.ts
+test('It works', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new JoelitoDevInfra.JoelitoDevInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
